8704d6fe76ddebb806e218fe22d01edf4b0eec96
Dario <darioorfano@hotmail.com>
2023-11-18 11:45:06 -0300

    Se agrego configuracion de rutas para renderizar las vistas correspondientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
+import Notebook from "./pages/Notebook";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/notebook" element={<Notebook />} />
+        <Route path="/" element={<Home />} />
+        {/* Puedes agregar un Route para '/soporte-tecnico' si es necesario */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Notebook.jsx
@@ -0,0 +1,16 @@
+import CardItem from "../components/CardItem"
+import Footer from "../components/Footer"
+import Navbar from "../components/navbar"
+
+const Notebook = () => {
+  return (
+    <>
+      <Navbar/>
+      <CardItem/>
+
+      <Footer/>
+      </>
+  )
+}
+
+export default Notebook
