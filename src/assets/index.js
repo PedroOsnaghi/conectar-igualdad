d0216b0ef73bdbe3e490c1d9f34fe2fc97f86237
Pedro Osnaghi <osnaghi.prog@gmail.com>
2023-11-18 15:40:51 -0300

    Menu inicio

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,7 @@
 import dell from "./dell.webp";
 import cardImageExample from "./cardimage.jpg";
-<<<<<<< Updated upstream
-import top from "./top2.png";
-import logoME from "./logoME.png";
-=======
 import top from "./top2.webp";
 import logoME from "./LogoME.png";
->>>>>>> Stashed changes
 import logoArgentinaUnida from "./LogoArgentinaUnida.png";
 import topThumb from "./top-thumb.webp";
 
