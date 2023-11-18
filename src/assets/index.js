2c72f888afa8fc8aeeebbcb10b36a0a4973a4fe7
Pedro Osnaghi <osnaghi.prog@gmail.com>
2023-11-18 09:45:45 -0300

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,6 +2,6 @@ import dell from "./dell.png";
 import cardImageExample from "./cardimage.jpg";
 import top from "./top2.png";
 import logoME from "./logoME.png";
-import logoArgentinaUnida from "./logoArgentinaUnida.png";
+import logoArgentinaUnida from "./LogoArgentinaUnida.png";
 
 export { dell, cardImageExample, top, logoME, logoArgentinaUnida };
